b932f9e1d66e79d2906fb448b6771fdc89fc6fac
“mohitkhare33” <mohitkhare26@gmail.com>
2021-12-12 05:26:23 +0530

    third

---
+++ TestController.java
@@ -0,0 +1,53 @@
+package com.app.registration.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.registration.model.User;
+import com.app.registration.service.RegistrationService;
+
+@RestController
+public class RegistrationController {
+
+	
+	@Autowired
+	private RegistrationService service;
+	
+	@PostMapping("/registeruser")
+	public User registerUser(@RequestBody User user) throws Exception
+	{
+		String tempEmailId= user.getEmailId();
+		if(tempEmailId !=null && !"".equals(tempEmailId))
+		{
+			User userObj =service.fetchUserByEmailId(tempEmailId);
+			if(userObjssss!=null)
+			{
+				throw new okException("user with  "+tempEmailId+" is alredy exist");
+			}
+		}
+		User userObj=null;
+		userObj=service.saveUser(user);
+		return userObj;
+	}
+	
+	@PostMapping("/login")
+	public User loginUser(@RequestBody User user) throws Exception
+	{
+		String tempEmailId=user.getEmailId();
+		String tempPass=user.getPassword();
+		User userobj =null;
+		if(tempEmailId != null && tempPass != null)
+		{
+			userobj =service.fetchUserByEmailIdAndPassword(tempEmailId, tempPass);
+		
+		}
+		if(userobj == null)
+		{
+			throw new Exception("Bad creadentials");
+		}
+		
+		return userobj;
+	}
+}
